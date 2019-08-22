d48c166de508e57c9131e73565e722f1f736ec81
wayne <cymatics@foxmail.com>
2019-08-22 17:36:04 +0800

    1.0.0

---
+++ app/src/main/java/com/example/wayne/dentist/mvp/Preselection/PreselectionPre.java
@@ -0,0 +1,16 @@
+package com.example.wayne.dentist.mvp.Preselection;
+
+
+import com.example.wayne.dentist.base.BasePre;
+
+/**
+ * @name 张东
+ * @time wayne2019/8/2
+ * @describe
+ */
+public class PreselectionPre extends BasePre<PreselectionView> {
+    @Override
+    protected void initModel() {
+
+    }
+}
